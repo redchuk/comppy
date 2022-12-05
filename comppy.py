84b9e0cd1bd5a22a8ad10bd2a195f30370b13737
Taras Redchuk <taras.redchuk@gmail.com>
2022-12-05 15:57:37 +0200

    crop around input click

--- comppy.py
+++ comppy.py
@@ -2,23 +2,29 @@ import glob
 from skimage import io
 from matplotlib import pyplot as plt
 
-i_path = 'input/plate2_B01_s3_w13F8D900F-FF8D-440D-921D-51C21118670B.tif'
-
+i_path = 'input/*'
 
 for i in glob.glob(i_path):
-
     filename = str(i).split('\\')[-1]
-    print (filename)
-    stack = io.imread(i)
-    plt.imshow(stack)
-    x = plt.ginput(2)
+    print(filename)
+    im = io.imread(i)
+    mng = plt.get_current_fig_manager()
+    mng.window.state('zoomed')
+    plt.imshow(im)
+    x = plt.ginput(1)
     print(x)
     plt.close()
 
-    plt.imshow(stack)
+    plt.imshow(im[int(x[0][1])-100:int(x[0][1])+100, int(x[0][0])-100:int(x[0][0])+100])
+    plt.waitforbuttonpress(timeout=5)
+    plt.close()
+
+
+'''
+    plt.imshow(im)
     plt.annotate('yay!', x[0])
     plt.waitforbuttonpress(timeout=5)
 
     plt.show()
     plt.close()
-    break
+'''
