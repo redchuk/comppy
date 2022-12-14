a2bbdd21037610c29807786c3e8ce119e29dc777
Taras Redchuk <taras.redchuk@gmail.com>
2022-12-14 16:00:41 +0200

    now it's a true square

--- comppy.py
+++ comppy.py
@@ -10,7 +10,7 @@ for i in glob.glob(i_path):
     print(filename + channel)
     im = io.imread(i)
     mng = plt.get_current_fig_manager()  # to get fullscreen image
-    mng.window.state('zoomed')           # to get fullscreen image
+    mng.window.state('zoomed')  # to get fullscreen image
     plt.imshow(im)
     x = plt.ginput(2)
     print(x)
@@ -22,12 +22,32 @@ for i in glob.glob(i_path):
     min_y = min(int(x[0][0]), int(x[1][0]))
     max_y = max(int(x[0][0]), int(x[1][0]))
 
-    plt.imshow(im[min_x:max_x, min_y:max_y])
+    center_x = min_x + int((max_x - min_x) / 2)
+    center_y = min_y + int((max_y - min_y) / 2)
+
+    print('cx'+str(center_x))
+    print('cy' + str(center_y))
+
+    diff_x = max_x - min_x
+    diff_y = max_y - min_y
+
+    print('dx'+str(diff_x))
+    print('dy' + str(diff_y))
+
+
+    crop_size = max(diff_x, diff_y)
+    print(crop_size)
+
+
+    crop = im[(center_x - int(crop_size / 2)):(center_x + int(crop_size / 2)),
+              (center_y - int(crop_size / 2)):(center_y + int(crop_size / 2))]
+    print(crop.shape)
+
+    plt.imshow(crop)
     plt.waitforbuttonpress(timeout=5)
     plt.close()
 
     break
-
 '''
     plt.imshow(im)
     plt.annotate('yay!', x[0])
