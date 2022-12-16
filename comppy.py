dfbb4300e4ff629e5cd15fe05986394ed1c1da66
Taras Redchuk <taras.redchuk@gmail.com>
2022-12-16 14:52:23 +0200

    contrast both green and cropped

--- comppy.py
+++ comppy.py
@@ -4,7 +4,6 @@ from skimage import exposure
 from matplotlib import pyplot as plt
 import numpy as np
 
-
 i_path = 'input/*'
 well = input('well number:')
 well_list = []
@@ -17,47 +16,49 @@ for i in glob.glob(i_path):
 
 for i in well_list:
     if '2' in i.split('_')[-1][1]:  # channel
-        #print(i)
+        # print(i)
         green_list.append(i)
 
 for i in green_list:
     print(i)
     im = io.imread('input/' + i)
+    g_perc = np.percentile(im, 99.95)
+    g_contr = exposure.rescale_intensity(im, in_range=(50, g_perc))
     mng = plt.get_current_fig_manager()  # to get fullscreen image
     mng.window.state('zoomed')  # to get fullscreen image
-    plt.imshow(im)
+    plt.imshow(g_contr)
     x = plt.ginput(2, mouse_add=1, mouse_stop=3)  # 1 for left click, 2 for right
     print(x)
     plt.close()
 
-    # having two clicks to set square we need to get min and max for x and y
-    min_x = min(int(x[0][1]), int(x[1][1]))
-    max_x = max(int(x[0][1]), int(x[1][1]))
-    min_y = min(int(x[0][0]), int(x[1][0]))
-    max_y = max(int(x[0][0]), int(x[1][0]))
+    if x:
+        # having two clicks to set square we need to get min and max for x and y
+        min_x = min(int(x[0][1]), int(x[1][1]))
+        max_x = max(int(x[0][1]), int(x[1][1]))
+        min_y = min(int(x[0][0]), int(x[1][0]))
+        max_y = max(int(x[0][0]), int(x[1][0]))
 
-    center_x = min_x + int((max_x - min_x) / 2)
-    center_y = min_y + int((max_y - min_y) / 2)
+        center_x = min_x + int((max_x - min_x) / 2)
+        center_y = min_y + int((max_y - min_y) / 2)
 
-    diff_x = max_x - min_x
-    diff_y = max_y - min_y
+        diff_x = max_x - min_x
+        diff_y = max_y - min_y
 
-    crop_size = max(diff_x, diff_y)
+        crop_size = max(diff_x, diff_y)
 
-    crop = im[(center_x - int(crop_size / 2)):(center_x + int(crop_size / 2)),
-           (center_y - int(crop_size / 2)):(center_y + int(crop_size / 2))]
+        crop = im[(center_x - int(crop_size / 2)):(center_x + int(crop_size / 2)),
+                  (center_y - int(crop_size / 2)):(center_y + int(crop_size / 2))]
 
-    plt.imshow(crop)
-    plt.waitforbuttonpress(timeout=1)
-    plt.close()
+        plt.imshow(crop)
+        plt.waitforbuttonpress(timeout=0.5)
+        plt.close()
 
-    # Contrast stretching
-    perc = np.percentile(crop, 99.5)
-    contr = exposure.rescale_intensity(crop, in_range=(110, perc))  # or crop.max()?
+        # Contrast stretching
+        perc = np.percentile(crop, 99.5)
+        contr = exposure.rescale_intensity(crop, in_range=(90, perc))  # or crop.max()?
 
-    plt.imshow(contr, cmap='gray')
-    plt.waitforbuttonpress(timeout=1)
-    plt.close()
+        plt.imshow(contr, cmap='gray')
+        plt.waitforbuttonpress(timeout=1)
+        plt.close()
 
     # to get pseudocolors convert to RGP via appropriate LUT then sum.
-
