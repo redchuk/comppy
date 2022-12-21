c81ce85649c43406c03c2d20dcf3f2e62d291e03
Taras Redchuk <taras.redchuk@gmail.com>
2022-12-21 14:08:20 +0200

    configure logging (just started)

--- comppy.py
+++ comppy.py
@@ -1,11 +1,26 @@
 import glob
+import os
 from skimage import io
 from skimage import exposure
 from matplotlib import pyplot as plt
 import numpy as np
+import logging
+import subprocess
+from datetime import datetime
 
+logging.basicConfig(level=logging.INFO)
 i_path = 'input/*'
-well = input('well number:')
+# todo: plateid
+well = input('well number(format B08):')
+protein = input('protein(format N_D377Y):')
+date = datetime.now().strftime('%Y%m%d')
+folder = '_'.join([protein,
+                   well,
+                   date,
+                   subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()  # git hash
+                   ])
+os.mkdir('output/' + folder)
+
 well_list = []
 green_list = []
 
@@ -19,9 +34,19 @@ for i in well_list:
         # print(i)
         green_list.append(i)
 
+
+def get_list_of_files(path, pattern_to_search):
+    list_of_files = []
+    for file in glob.glob(path):
+        if pattern_to_search in file:
+            list_of_files.append(file)
+    return list_of_files
+
+
 for i in green_list:
     print(i)
     im = io.imread('input/' + i)
+    fov = '_'.join(i.split('_')[:3]) + '_'
     g_perc = np.percentile(im, 99.95)
     g_contr = exposure.rescale_intensity(im, in_range=(50, g_perc))
     mng = plt.get_current_fig_manager()  # to get fullscreen image
@@ -31,6 +56,7 @@ for i in green_list:
     print(x)
     plt.close()
 
+    # todo: try instead of if + logging
     if x:
         # having two clicks to set square we need to get min and max for x and y
         min_x = min(int(x[0][1]), int(x[1][1]))
@@ -46,19 +72,20 @@ for i in green_list:
 
         crop_size = max(diff_x, diff_y)
 
-        crop = im[(center_x - int(crop_size / 2)):(center_x + int(crop_size / 2)),
-                  (center_y - int(crop_size / 2)):(center_y + int(crop_size / 2))]
+        for channel in get_list_of_files(i_path, fov):
+            im = io.imread(channel)
+            crop = im[(center_x - int(crop_size / 2)):(center_x + int(crop_size / 2)),
+                   (center_y - int(crop_size / 2)):(center_y + int(crop_size / 2))]
 
-        plt.imshow(crop)
-        plt.waitforbuttonpress(timeout=0.5)
-        plt.close()
+            plt.imshow(crop)
+            plt.waitforbuttonpress(timeout=0.5)
+            plt.close()
 
-        # Contrast stretching
-        perc = np.percentile(crop, 99.5)
-        contr = exposure.rescale_intensity(crop, in_range=(90, perc))  # or crop.max()?
+            # Contrast stretching
+            perc = np.percentile(crop, 99.5)
+            contr = exposure.rescale_intensity(crop, in_range=(90, perc))  # or crop.max()?
 
-        plt.imshow(contr, cmap='gray')
-        plt.waitforbuttonpress(timeout=1)
-        plt.close()
-
+            plt.imshow(contr, cmap='gray')
+            plt.waitforbuttonpress(timeout=1)
+            plt.close()
     # to get pseudocolors convert to RGP via appropriate LUT then sum.
