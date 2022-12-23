1502c240137490b9e0c5c9cebca7c6ee88cbf1eb
Taras Redchuk <taras.redchuk@gmail.com>
2022-12-23 12:34:45 +0200

    several crops from the same image, shows all channels simultaneously after cropping

--- comppy.py
+++ comppy.py
@@ -65,53 +65,65 @@ for i in green_list:
     im = io.imread('input/' + i)
     fov = '_'.join(i.split('_')[:3]) + '_'
     g_perc = np.percentile(im, 99.95)
-    g_contr = exposure.rescale_intensity(im, in_range=(50, g_perc))
-    mng = plt.get_current_fig_manager()  # to get fullscreen image
-    mng.window.state('zoomed')  # to get fullscreen image
-    plt.imshow(g_contr)
-    x = plt.ginput(2, mouse_add=1, mouse_stop=3)  # 1 for left click, 2 for right
-    plt.close()
+    g_contr = exposure.rescale_intensity(im, in_range=(100, g_perc))
+    x = ['dummy']
 
-    try:
-        # having two clicks to set square we need to get min and max for x and y
-        min_x = min(int(x[0][1]), int(x[1][1]))
-        max_x = max(int(x[0][1]), int(x[1][1]))
-        min_y = min(int(x[0][0]), int(x[1][0]))
-        max_y = max(int(x[0][0]), int(x[1][0]))
-
-        center_x = min_x + int((max_x - min_x) / 2)
-        center_y = min_y + int((max_y - min_y) / 2)
-
-        diff_x = max_x - min_x
-        diff_y = max_y - min_y
-
-        crop_size = max(diff_x, diff_y)
-
-        for ch_number, channel in enumerate(get_list_of_files(i_path, fov)):  # todo:
-
-            im = io.imread(channel)
-            crop = im[(center_x - int(crop_size / 2)):(center_x + int(crop_size / 2)),
-                      (center_y - int(crop_size / 2)):(center_y + int(crop_size / 2))]
-
-            plt.imshow(crop)
-            plt.waitforbuttonpress(timeout=0.5)
+    while x:
+        try:
+            mng = plt.get_current_fig_manager()  # to get fullscreen image
+            mng.window.state('zoomed')  # to get fullscreen image
+            plt.imshow(g_contr)
+            x = plt.ginput(2, mouse_add=1, mouse_stop=3)  # 1 for left click, 3 for right
             plt.close()
 
-            # Contrast stretching
-            contr = exposure.rescale_intensity(crop, in_range=(min_max_histo[ch_number][0],
-                                           np.percentile(crop, min_max_histo[ch_number][1])))
+            # having two clicks to set square we need to get min and max for x and y
+            min_x = min(int(x[0][1]), int(x[1][1]))
+            max_x = max(int(x[0][1]), int(x[1][1]))
+            min_y = min(int(x[0][0]), int(x[1][0]))
+            max_y = max(int(x[0][0]), int(x[1][0]))
 
-            plt.imshow(contr, cmap='gray')
-            plt.waitforbuttonpress(timeout=1)
+            center_x = min_x + int((max_x - min_x) / 2)
+            center_y = min_y + int((max_y - min_y) / 2)
+
+            diff_x = max_x - min_x
+            diff_y = max_y - min_y
+
+            crop_size = max(diff_x, diff_y)
+
+            plt.figure(figsize=(10, 10))
+
+            for ch_number, channel in enumerate(get_list_of_files(i_path, fov)):
+
+                im = io.imread(channel)
+                crop = im[(center_x - int(crop_size / 2)):(center_x + int(crop_size / 2)),
+                          (center_y - int(crop_size / 2)):(center_y + int(crop_size / 2))]
+
+                #plt.imshow(crop)
+                #plt.waitforbuttonpress(timeout=0.5)
+                #plt.close()
+
+                # Contrast stretching
+                contr = exposure.rescale_intensity(crop, in_range=(min_max_histo[ch_number][0],
+                                               np.percentile(crop, min_max_histo[ch_number][1])))
+
+                plt.subplot(1, 4, (ch_number+1))
+                plt.imshow(contr, cmap='gray')
+                plt.axis('off')
+                #plt.waitforbuttonpress(timeout=0.5)
+                #plt.close()
+
+                filename = '_'.join([protein,
+                                     plate_id,
+                                     well,
+                                     random_str,
+                                     ])
+            # to get more than 1 crop from an image
+            plt.tight_layout()
+            plt.show()
+            plt.waitforbuttonpress(timeout=10)
             plt.close()
 
-            filename = '_'.join([protein,
-                                 plate_id,
-                                 well,
-                                 random_str,
-                                 ])
-
-    except Exception:
-        logging.info('no proper input coordinates for cropping')
+        except Exception:
+            logging.info('no (more) proper input coordinates for cropping')
 
     # to get pseudocolors convert to RGP via appropriate LUT then sum.
